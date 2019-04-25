b5ef383084cd5e4dab3668a2279d08e637c3aed6
u15128271 <itumeleng.morei@gmail.com>
2019-04-25 11:59:56 +0200

    XamarinApp

---
+++ Desktop/SQLiteDB/SQLiteDB/Resources/Model/Person.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace SQLiteDB.Resources.Model
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+    }
+}
