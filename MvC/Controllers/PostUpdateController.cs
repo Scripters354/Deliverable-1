b5686f0feb9a849b34399998d67edb2e0f3de20c
Khayelihle Simelane <scripters.inf370@gmail.com>
2019-04-27 13:10:17 +0200

    MVC & views

---
+++ MvC/Controllers/PostUpdateController.cs
@@ -0,0 +1,22 @@
+﻿using MvC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvC.Controllers
+{
+    public class PostUpdateController : Controller
+    {
+        // GET: PostUpdate
+        public ActionResult Index()
+        {
+            IEnumerable<mvcPostUpdateModel> postList; 
+            HttpResponseMessage response = GlobalVariables.WepApiClient.GetAsync("Post_Update").Result;  
+            postList = response.Content.ReadAsAsync<IEnumerable<mvcPostUpdateModel>>().Result;
+            return View(postList);
+        }
+    }
+}
